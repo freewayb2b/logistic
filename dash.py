a40a251fd38887c367c2675cdedc04810d4f47e9
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-12 15:49:04 -0300

    a

--- dash.py
+++ dash.py
@@ -46,6 +46,8 @@ st.metric("Pago",f'R$ {total:,.2f}')
 st.dataframe(df_filtrado,use_container_width = True, hide_index = True)
 
 
+brasil = gpd.read_file('https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson')
+
 fig = px.choropleth(
     df_filtrado,
     locations='UF',  # Coluna no DataFrame contendo os nomes dos estados
