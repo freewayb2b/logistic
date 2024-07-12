b8d155443119c049975ec8f3185ab8d50f0e9b3d
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-12 14:40:12 -0300

    a

--- dash.py
+++ dash.py
@@ -24,7 +24,7 @@ link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_InxkV5GPZKYxQp1qO9d1k
 
 df_teste = pd.read_csv(link)
 
-st.write(df_teste["DATA N.F."].info)
+st.write(df_teste["DATA N.F."].month)
 
 # df_teste = df_teste.drop(columns=["TNT FRANCA","%","%.1","%.2","%.3","%.4","%.5","%.6","%.7","F.L. LOG.","TROCA TRANS.","VITLOG","RODO-NAVES","VR. FRETE CALCULADO N.F.","VR. DIFER."])
 
