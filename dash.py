6610c0e08875d2c5502540b18b625a51e7c72d1b
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-12 14:01:54 -0300

    a

--- dash.py
+++ dash.py
@@ -20,6 +20,6 @@ st.subheader("teste")
 
 link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_InxkV5GPZKYxQp1qO9d1knpB4_xzbh-TL0YYDor-wY1ldpmOisnRDZ6imGvt6d14rz8IRS7ivN3K/pub?output=xlsx"
 
-df_teste = pd.read_excel(link)
+df_teste = pd.read_csv(link)
 
 st.dataframe(df_teste,use_container_width = True, hide_index = True)
