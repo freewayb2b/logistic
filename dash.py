7b80ca8ef18a0d83f1e0d28cfebacd88b865b5c3
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-12 13:39:19 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -7,10 +7,12 @@ import datetime as dt
 #-----------------------------------------------------------------------------------------------------
 #ETL
 
-gc3 = sg.service_account("logistica.json")
-link = "https://docs.google.com/spreadsheets/d/1hnV9zOAG33fFhw97RX7RERAaFDn63zKXalEJg9EJcsI/edit?usp=sharing"
-sh = gc.open_by_url(link)
-ws = sh.get_worksheet(0)
-planilha = ws.get_all_values()
-df_teste = pd.DataFrame(planilha[1:], columns=planilha[0])
-st.dadaframe(df_teste,use_container_width = True, hide_index = True)
+# gc3 = sg.service_account("logistica.json")
+# link = "https://docs.google.com/spreadsheets/d/1hnV9zOAG33fFhw97RX7RERAaFDn63zKXalEJg9EJcsI/edit?usp=sharing"
+# sh = gc.open_by_url(link)
+# ws = sh.get_worksheet(0)
+# planilha = ws.get_all_values()
+# df_teste = pd.DataFrame(planilha[1:], columns=planilha[0])
+# st.dadaframe(df_teste,use_container_width = True, hide_index = True)
+
+st.subheader("teste")
