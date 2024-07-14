8d6e8e8c01555b81c2aa67ba085d115c41758311
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-07-14 20:05:57 -0300

    Add files via upload

--- dash.py
+++ dash.py
@@ -1,48 +1,120 @@
+import folium.map
 import streamlit as st
 import pandas as pd
-import gspread as sg
-from gspread import Worksheet
 import datetime as dt
-import geopandas as gpd
-import matplotlib.pyplot as plt
 import plotly.express as px
+from streamlit_folium import st_folium
+
+#-----------------------------------------------------------------------------------------------------
+#page config
+
+st.set_page_config(layout = "wide",page_title="Logística FW",page_icon="🚚")
+
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html = True)
+
+st.title("Visão Geral - Logística Freeway", anchor= False)
+st.divider()
+
+col1, col2, col3, col4, col5 = st.columns([2,2,2,1,1])
+col6, col7= st.columns(2)
 
 
-st.set_page_config(layout = "wide")
+
 #-----------------------------------------------------------------------------------------------------
 #ETL
 
-# gc = sg.service_account("logistica.json")
-# link = "https://docs.google.com/spreadsheets/d/1hnV9zOAG33fFhw97RX7RERAaFDn63zKXalEJg9EJcsI/edit?usp=sharing"
-# sh = gc.open_by_url(link)
-# ws = sh.get_worksheet(0)
-# planilha = ws.get_all_values()
-# df_teste = pd.DataFrame(planilha[1:], columns=planilha[0])
-# st.dataframe(df_teste,use_container_width = True, hide_index = True)
-
-st.subheader("Logística - Freeway",anchor = False)
-
-
 link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_InxkV5GPZKYxQp1qO9d1knpB4_xzbh-TL0YYDor-wY1ldpmOisnRDZ6imGvt6d14rz8IRS7ivN3K/pub?output=csv"
 
-df_teste = pd.read_csv(link)
-df_teste['Data'] = pd.to_datetime(df_teste["DATA N.F."])
-df_teste["Mês"] = df_teste["Data"].dt.month
+df = pd.read_csv(link)
+
+df["Data"] = pd.to_datetime(df["DATA N.F."])
+df["Mês"] = pd.to_datetime(df["DATA N.F."]).dt.day
+df["dia"] = pd.to_datetime(df["DATA N.F."]).dt.month
+df["Ano"] = df["Data"].dt.year
+df['data_string'] = df['Ano'].astype(str) + '-' + df['Mês'].astype(str).str.zfill(2) + '-' + df['dia'].astype(str).str.zfill(2)
 
 
-df_teste = df_teste.drop(columns=["TNT FRANCA","%","%.1","%.2","%.3","%.4","%.5","%.6","%.7","F.L. LOG.","TROCA TRANS.","VITLOG","RODO-NAVES","VR. FRETE CALCULADO N.F.","VR. DIFER."])
+df = df.drop(columns=["TNT FRANCA","%","%.1","%.2","%.3","%.4","%.5","%.6","TNT JACOBINA","MENOR VR. FRETE","TRANSP. MENOR FRETE",'data_string',"Data",
+                "%.7","F.L. LOG.","TROCA TRANS.","VITLOG","RODO-NAVES","VR. FRETE CALCULADO N.F.","VR. DIFER."])
 
-df_teste['VR. FRETE COBRADO'] = df_teste['VR. FRETE COBRADO'].str.replace('.', '').str.replace(',', '.').astype(float)
+df['FRETE PAGO'] = df['VR. FRETE COBRADO'].str.replace('.', '').str.replace(',', '.').astype(float)
+df['VALOR N.FISCAL'] = df['VALOR N.FISCAL'].str.replace('.', '').str.replace(',', '.').astype(float)
 
 
 
-filto_trans = st.selectbox("",df_teste["TRANSPORTADORA"].unique())
+#-----------------------------------------------------------------------------------------------------
+geo_local = pd.read_json('uf.json',encoding='latin-1')
+geo_estados = pd.read_csv('data.csv',encoding= 'latin-1')
 
-df_filtrado = df_teste.query('TRANSPORTADORA == @filto_trans')
+#-----------------------------------------------------------------------------------------------------
+#mapear meses
 
-total = df_filtrado["VR. FRETE COBRADO"].sum()
-st.metric("Pago",f'R$ {total:,.2f}')
+mapear_meses = {
+        1:'Jan',
+        2:'Fev',
+        3:'Mar',
+        4:'Abr',
+        5:'Mai',
+        6:'Jun',
+        7:'Jul',
+        8:'Ago',
+        9:'Set',
+        10:'Out',
+        11:'Nov',
+        12:'Dez'
+}
+df = df.sort_values('Mês', ascending= True)
 
+df["Mês"] = df["Mês"].map(mapear_meses)
+
+
+#-----------------------------------------------------------------------------------------------------
+#filters
+
+with col4:
+    filter_year = st.selectbox('Ano', df["Ano"].unique())
+with col5:
+    filter_month = st.selectbox('Mês',df["Mês"].unique())
+
+df_filtrado = df.query('Ano == @filter_year and Mês == @filter_month')
+df_filtrado = df_filtrado.drop(columns=["Ano","VR. FRETE COBRADO","VR. FRETE COTAÇAO"])
+
+
+qtd_nfs = df_filtrado.shape[0]
+total = df_filtrado["VALOR N.FISCAL"].sum()
+valor_frete = df_filtrado["FRETE PAGO"].sum()
+
+with col1:
+    st.metric("QTD NFs",qtd_nfs)
+    
+with col2:
+    st.metric("Total Faturado",f'R$ {total:,.0f}')
+    
+with col3:
+    st.metric("Total Fretes",f'R$ {valor_frete:,.0f}')
+
+#-----------------------------------------------------------------------------------------------------
+#charts 
+df_columns = df_filtrado.groupby('dia')['FRETE PAGO'].sum().reset_index()
+columns_chart = px.area(df_columns,x="dia", y="FRETE PAGO",title="Acompanhamento Diário")
+
+with col6:
+    st.plotly_chart(columns_chart,use_container_width= True)
+    
+
+ 
 st.dataframe(df_filtrado,use_container_width = True, hide_index = True)
+#-----------------------------------------------------------------------------------------------------
 
 
+# # Exiba o mapa no Streamlit
+# st.title('Mapa de Choropleth com Folium no Streamlit')
+
+
+
+
+# m = folium.Map(location=[-12.2350,-51.9253], zoom_start=4)
+
+
+# st_map = st_folium(m, width=700, height=500)
