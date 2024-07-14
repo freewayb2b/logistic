d5769f4760f8a3be3c069122f39a6459c2f2e3f8
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-07-14 20:19:20 -0300

    A

--- dash.py
+++ dash.py
@@ -97,7 +97,7 @@ with col6:
     st.plotly_chart(area_chart,use_container_width= True)
 
 df_bar = df_filtrado.groupby('TRANSPORTADORA')['FRETE PAGO'].sum().reset_index()
-bar_chart = px.bar(df_bar,x="TRANSPORTADORA", y="FRETE PAGO",title="Frete Por Transportadora",orientation= "v")
+bar_chart = px.bar(df_bar,x="FRETE PAGO", y="TRANSPORTADORAA",title="Frete Por Transportadora",orientation= "h")
 with col7:
     st.plotly_chart(bar_chart,use_container_width= True)
     a
