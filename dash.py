d6f36f973e158146edc7d0deae851a3f93ff549b
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-15 11:14:56 -0300

    a

--- dash.py
+++ dash.py
@@ -180,6 +180,7 @@ df_filtrado["DATA N.F."] = df_filtrado["DATA N.F."].dt.strftime('%d/%m/%Y')
 #-----------------------------------------------------------------------------------------------------
 
 df_uf = df_filtrado.groupby(['UF'])['FRETE PAGO'].sum().reset_index()
+df_uf = df_uf.sort_values('FRETE PAGO',ascending=True)
 
 with col9:
     st.subheader("Acompanhamento", anchor = False)
