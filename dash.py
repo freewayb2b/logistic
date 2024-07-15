a76b7b5759532c06d7a8305d326efae27c31fb9e
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-15 09:49:29 -0300

    a

--- dash.py
+++ dash.py
@@ -122,9 +122,9 @@ with col7:
 df_faturamento = df_filtrado.groupby('dia')['VALOR N.FISCAL'].sum().reset_index()
 
 area_chart_faturamento = px.bar(df_faturamento,x="dia", y="VALOR N.FISCAL",
-# text=df_faturamento['VALOR N.FISCAL'].apply(lambda x: f'R$ {x:,.0f}'),
+    text=df_faturamento['VALOR N.FISCAL'].apply(lambda x: f'R$ {x:,.0f}'),
             title="Faturamento Diário",color_discrete_sequence=[cor_barras])
-area_chart_faturamento.update_xaxes(dtick=1)
+# area_chart_faturamento.update_xaxes(dtick=1)
 area_chart_faturamento.update_traces(showlegend=False,textfont=dict(size=20,color='#ffffff'),textposition='auto')
 area_chart_faturamento.layout.xaxis.fixedrange = True
 area_chart_faturamento.layout.yaxis.fixedrange = True
