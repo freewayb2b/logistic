2d611b767017ce86fd09ffb208c2bafeff907619
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-07-15 22:54:52 -0300

    a

--- dash.py
+++ dash.py
@@ -11,7 +11,7 @@ st.set_page_config(layout = "wide",page_title="Logística FW",page_icon="🚚")
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html = True)
 
-st.title("Logística - Freeway", anchor= False)
+st.title("Visão Geral Logística - Freeway", anchor= False)
 st.divider()
 
 
