43b536fb4bd1153676f75d1dccf9ba09d3e40268
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-17 15:45:02 -0300

    a

--- dash.py
+++ dash.py
@@ -181,7 +181,7 @@ with col8:
 df_pie = df_filtrado.groupby('UNIDADE')['FRETE PAGO'].sum().reset_index()    
 
 pie_chart = px.pie(df_pie,values = "FRETE PAGO", names ="UNIDADE",title = "Frete por Unidade",
-category_orders={'UNIDADE':["Franca","Jacobina"]},color_discrete_sequence=["#642705","#BC693A"])
+category_orders = {'UNIDADE':["Franca","Jacobina"]},color_discrete_sequence=["#642705","#BC693A"])
 pie_chart.update_traces(showlegend=True,textfont=dict(size=17,color='#ffffff'),textposition='outside')
 
 
