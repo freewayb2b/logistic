b791e63badd327f786c3eb7d8be8433e42dc4c78
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-17 08:14:45 -0300

    a

--- dash.py
+++ dash.py
@@ -30,7 +30,8 @@ link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_InxkV5GPZKYxQp1qO9d1k
 df = pd.read_csv(link)
 
 # df["Data"] = pd.to_datetime(df["DATA N.F."]).dt.strftime('%d/%m/%Y')
-st.table(df)
+with col9:
+    st.table(df)
 # df["Mês"] = pd.to_datetime(df["DATA N.F."]).dt.month
 # df["dia"] = pd.to_datetime(df["DATA N.F."]).dt.day
 # df["Ano"] = df["Data"].dt.year
