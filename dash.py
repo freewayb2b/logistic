895844b309c00d9036c1e19f3f1dae99cadafef6
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-19 14:25:43 -0300

    a

--- dash.py
+++ dash.py
@@ -245,7 +245,7 @@ with col10:
     st.dataframe(df_filtrado,use_container_width = True, hide_index = True)
 
 with col11:
-    uf_chart = px.bar(df_uf,x="UF",y="FRETE PAGO",orientation = "v")
+    uf_chart = px.bar(df_uf,x="UF",y="FRETE PAGO",orientation = "h")
     st.plotly_chart(uf_chart) 
     
 #-----------------------------------------------------------------------------------------------------
