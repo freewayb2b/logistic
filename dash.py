835dcab347de625f7006ee09494e85e7025967c4
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-08-17 22:03:01 -0300

    s

--- dash.py
+++ dash.py
@@ -184,7 +184,7 @@ with col3:
     st.metric("Frete Pago",f'🚚 R$ {valor_frete:,.0f}')
 
 with col4:
-    st.metric("Percentual Frete",f'💸 {percentual_frete}')
+    st.metric("Percentual Frete",f'💵 {percentual_frete}')
 
 #-----------------------------------------------------------------------------------------------------
 #charts colors
